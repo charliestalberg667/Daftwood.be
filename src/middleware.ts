48812473ceda5e93d58579a6ac86ad9649d4d44c
charlie <charlie.stalberg@icloud.com>
2025-06-28 21:49:56 +0200

    fix: remove unused param in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,8 @@
-import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
  
 
  
-export function middleware(_request: NextRequest) {
+export function middleware() {
   return NextResponse.next();
 }
  
