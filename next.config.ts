5254d34ed0303c8f5140c8891cc5cf83015c89ac
charlie <charlie.stalberg@icloud.com>
2025-06-28 10:55:53 +0200

    start

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import withNextIntl from 'next-intl/plugin';
+import type { NextConfig } from 'next';
+
+const withNextIntlPlugin = withNextIntl();
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withNextIntlPlugin(nextConfig);
