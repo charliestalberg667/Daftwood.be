1095ff467c00556e65c36d199867b4021bea82cd
charlie <charlie.stalberg@icloud.com>
2025-06-28 11:06:51 +0200

    start

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,19 +1,15 @@
-import { getRequestConfig } from 'next-intl/server';
-import { notFound } from 'next/navigation';
-
-type Locale = 'en' | 'nl' | 'fr';
-const locales: Locale[] = ['en', 'nl', 'fr'];
-
-export default getRequestConfig(async ({ locale }) => {
+import {notFound} from 'next/navigation';
+import {getRequestConfig} from 'next-intl/server';
+ 
+// Can be imported from a shared config
+const locales = ['en', 'nl', 'fr'];
+ 
+export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as Locale)) {
-    notFound();
-  }
-
+  if (!locales.includes(locale as any)) notFound();
+ 
   return {
-    messages: (await import(`./messages/${locale}.json`)).default,
-    locale: locale as Locale,
-    now: new Date(),
-    timeZone: 'Europe/Brussels',
+    messages: (await import(`../messages/${locale}.json`)).default,
+    locale: locale!
   };
 });
