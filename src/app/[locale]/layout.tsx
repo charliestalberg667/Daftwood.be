d6241e525179a80c8b48988597242499a5992a35
charlie <charlie.stalberg@icloud.com>
2025-06-29 05:37:14 +0200

    start

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -59,7 +59,7 @@ export default async function RootLayout({
   let messages;
   try {
     messages = (await import(`../../messages/${locale}.json`)).default;
-  } catch (_) {
+  } catch {
     notFound();
   }
 
